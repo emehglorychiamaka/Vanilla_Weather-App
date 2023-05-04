6a31eb1a005c122eff23deab68b898382e64e572
emehglorychiamaka <emehglorychiamaka@gmail.com>
2023-05-04 16:33:25 +0100

    entered the longitude and latitude

--- src/index.js
+++ src/index.js
@@ -65,13 +65,13 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
-function getForecast(position) {
-  console.log(position);
+function getForecast(coordinates) {
+  console.log(coordinates);
   let apiKey = "889c7f3ed9dacba9e272eab879e0a867";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
+  /*let lat = position.coords.latitude;
+  let lon = position.coords.longitude;*/
   let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayForecast);
 }
 
